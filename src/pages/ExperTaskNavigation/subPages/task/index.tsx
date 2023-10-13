5da3fdd00cf7d5158f83b1cc10c53b8e4f7ad706
zhaoyuechun11 <1591571013@qq.com>
2023-10-13 11:26:40 +0800

    优化前处理和专业组页面

--- src/pages/ExperTaskNavigation/subPages/task/index.tsx
+++ src/pages/ExperTaskNavigation/subPages/task/index.tsx
@@ -1,29 +1,10 @@
 import React from 'react';
-import { Tabs } from 'antd';
-
 import Pretreatment from './commones/pretreatment';
-import Chemistry from './commones/chemistry';
-import s from './index.less';
 import Immunity from './commones/immunity';
 import { useSelector } from 'umi';
-const { TabPane } = Tabs;
+
 const Task = () => {
   const { useDetail } = useSelector((state: any) => state.global);
-  return (
-    <div className={s.card_container}>
-      {useDetail.departmentType === '前处理组' ? <Pretreatment /> : <Immunity />}
-      {/* <Tabs defaultActiveKey="1" type="card">
-        <TabPane tab="前处理任务汇总" key="1">
-          <Pretreatment />
-        </TabPane>
-        <TabPane tab="普通检验流程(化学发光)" key="2">
-          <Chemistry />
-        </TabPane>
-        <TabPane tab="普通检验流程(免疫)" key="3">
-          <Immunity />
-        </TabPane>
-      </Tabs> */}
-    </div>
-  );
+  return <div>{useDetail.departmentType === '前处理组' ? <Pretreatment /> : <Immunity />}</div>;
 };
 export default Task;
